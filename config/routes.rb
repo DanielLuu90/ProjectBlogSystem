a28cde754b1ad8d1550da277f346cc0d0e7f64c6
TrucLuu <truclh@hotmail.com>
2015-06-15 20:41:40 +0700

    Update User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   get "home" => 'static_pages#home'
 
   get "about" => 'static_pages#about'
@@ -6,6 +7,9 @@ Rails.application.routes.draw do
   get "contact" => 'static_pages#contact'
 
   get "help" => 'static_pages#help'
+
+  get "signup" => 'users#new'
+  get "edit" => 'users#edit'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
